80c9dee8b0a33a2998db0b6e28459cf26929a7ef
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-10-25 23:43:16 +0530

    svelte structure setup

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,7 @@
+<form class="my-6">
+    <div class="flex flex-col text-sm mb-2">
+        <label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+        <input type="text" name="todo" placeholder="what to do?" class="shadow-sm border-gray-800 border-2 p-2 rounded-lg w-auto">
+    </div>
+    <button type="submit" class="w-min-content shadow-sm rounded-xl bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 border-gray-500 border-2">Submit</button>
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+import TodoForm from "../components/TodoForm.svelte";
+</script>
+
+<main>
+	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">MY todos</h1>
+	<TodoForm/>
+</main>
